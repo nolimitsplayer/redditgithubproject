252210f8c0c86d6303a175b08e6dc219c5eff109
Ryan <nolimitsplayer@gmail.com>
2012-10-10 15:15:41 -0700

    Working on printmatrix

--- Matrix.java
+++ Matrix.java
@@ -19,12 +19,22 @@ public class Matrix {
 
 	public Matrix determinantOfMatrix(Matrix a) {
 		if (a.rows() == a.columns()) {
+			int columns = a.columns();
+			for (int i=0; i<columns; i++) {
+				matrix[i]-
+			}
 			//do determinant
 		} else {
 			System.out.println("Determinant is not square.");
 		}
 	}
 
+	//row echelon form
+	public Matrix ref(Matrix a) {
+
+	}
+
+	//reduced row echelon form
 	public Matrix rref(Matrix a) {
 	}
 
@@ -37,6 +47,11 @@ public class Matrix {
 	}
 
 	public String printMatrix(Matrix a) {
-		
+		String result = "";
+		result += "[";
+		for (int i=0; i<a.rows(); i++)
+			for (int j=0; j<a.columns()-1; i++)
+				result += matrix[i][j];
+		return result;
 	}
 }
